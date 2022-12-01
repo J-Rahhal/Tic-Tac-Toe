dd5979e557acc844be1c71819af3e0bfad657638
dev-acc-practice <development-practice@proton.me>
2022-12-01 20:19:03 +0200

    styling layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        't': 'repeat(3, auto)'
+      }
+    },
   },
   plugins: [],
 }
